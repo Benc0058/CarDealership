76d92b8aed9d398171ac38af9ae1fbe1a67d74dd
Bence Eros <benc0058@edu.easj.dk>
2016-05-20 16:55:41 +0200

    First Step

---
+++ CarDealership/Model/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealership.Model
+{
+    public class User
+    {
+        private string _userName;
+        private string _password;
+
+        public string UserName
+        {
+            get
+            {
+                return _userName;
+            }
+
+            set
+            {
+                _userName = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+
+            set
+            {
+                _password = value;
+            }
+        }
+    }
+}
