85639f9866c7a0a96ec6c76759f6fc2401a95c2c
Bence Eros <benc0058@edu.easj.dk>
2016-05-24 17:15:22 +0200

    cleaning

--- CarDealership/View/RegisterCarPage.xaml.cs
+++ CarDealership/View/RegisterCarPage.xaml.cs
@@ -20,9 +20,9 @@ namespace CarDealership.View
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class RegisterCarPage : Page
     {
-        public MainPage()
+        public RegisterCarPage()
         {
             this.InitializeComponent();
         }
