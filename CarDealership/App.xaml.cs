68e1afcb8b8b39ac1f1b14f36675b183e8dddcfe
Bence Eros <benc0058@edu.easj.dk>
2016-05-27 17:52:27 +0200

    update

--- CarDealership/App.xaml.cs
+++ CarDealership/App.xaml.cs
@@ -44,10 +44,10 @@ namespace CarDealership
         protected override void OnLaunched(LaunchActivatedEventArgs e)
         {
 #if DEBUG
-            if (System.Diagnostics.Debugger.IsAttached)
-            {
-                this.DebugSettings.EnableFrameRateCounter = true;
-            }
+            //if (System.Diagnostics.Debugger.IsAttached)
+            //{
+            //    this.DebugSettings.EnableFrameRateCounter = true;
+            //}
 #endif
             Frame rootFrame = Window.Current.Content as Frame;
 
